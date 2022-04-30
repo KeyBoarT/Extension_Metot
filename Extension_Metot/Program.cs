903d257ca1c1faf4923a840be1c9f8f39e2a7dfc
Mehmet Sezer <mehmetszr05@gmail.com>
2022-04-30 23:06:07 +0300

    Proje dosyası ekle.

---
+++ Extension_Metot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Extension_Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ters Çevirmek istediğiniz kelimeyi giriniz : ");
+            string metin = Console.ReadLine();
+            Console.WriteLine("Ters dönmüş metin = " + metin.Reverse());
+            Console.ReadLine();
+        }
+    }
+
+    static class Extension
+    {
+        public static string Reverse(this string metin)
+        {
+            string yeniMetin = "";
+            for (int i = metin.Length - 1; i >= 0; i--)
+            {
+                yeniMetin += metin[i];
+            }
+            return yeniMetin;
+        }
+    }
+}
